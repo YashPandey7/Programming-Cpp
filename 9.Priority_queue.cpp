0ab6a9dacca1f89d3661fa71ed0c6fc3d5be7409
Yash Pandey <yashdevesh10@gmail.com>
2023-01-31 17:45:04 +0530

    Priority queue

---
+++ 9.Priority_queue.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	priority_queue <int> pq;
+	pq.push(10);
+	pq.push(15);
+	pq.push(5);
+	
+//	Max - Heap
+	cout<<"Max-Heap  --  ";
+	while(pq.empty()==false)
+	{
+		cout<<pq.top()<<" ";
+		pq.pop();
+	}
+	
+	cout<<endl;
+	priority_queue <int, vector<int>, greater<int>> pq1;
+	pq1.push(10);
+	pq1.push(15);
+	pq1.push(5);
+	
+//	Mix - Heap
+	cout<<"Min-Heap  --  ";
+	while(pq1.empty()==false)
+	{
+		cout<<pq1.top()<<" ";
+		pq1.pop();
+	}
+	
+//	Sort array using priority queue
+	cout<<"\nSorted array  --  ";
+	int arr[] = {1,3,2,6,5,4};
+	priority_queue <int, vector<int>, greater<int>> pq2(arr, arr+6);
+	while(pq2.empty()==false)
+	{
+		cout<<pq2.top()<<" ";
+		pq2.pop();
+	}
+	
+	return 0;
+}
