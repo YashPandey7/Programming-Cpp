6f14982f686977dec90f2ff9f4604a319e3018f8
Yash Pandey <yashdevesh10@gmail.com>
2023-02-04 23:55:54 +0530

    adding0

--- 4.vector_questions.cpp
+++ 4.vector_questions.cpp
@@ -1,23 +1,52 @@
 #include<iostream>
 #include<vector>
+#include<tuple>
 #include <algorithm>
+#include<utility>
 using namespace std;
 
 int main()
 {
-	int arr[] ={1,2,2,1,1};
-	int x=1,c=0;
-	vector <int> v (arr,arr+5);
-	sort(v.begin(),v.end());
+	int n=4;
+	int a[] = {1,2,1,2};
+	int b[] = {3,4,4,4};
+	int c[] = {1,3,2,4};
 	
-	for(int i=0; i<v.size(); i++)
+	vector <tuple <int,int,int>> t[n];
+	for(int i=0; i<n; i++)
 	{
-		if(v[i] == 1)
-		{
-			c++;
-		}
+		t[i].push_back(make_tuple(a[i], b[i], c[i]));
 	}
-	cout<<c;
+	
+	sort(t.begin(), t.end());
+	
+	for(int i=0; i<4 ; i++)
+	{
+		cout<< get<0>(t[i])<<" ";
+	}cout<<endl;
+	
+	for(int i=0; i<4 ; i++)
+	{
+		cout<< get<1>(t[i])<<" ";
+	}cout<<endl;
+	
+	for(int i=0; i<4 ; i++)
+	{
+		cout<< get<2>(t[i])<<" ";
+	}
+	
+//	int x=1,c=0;
+//	vector <int> v (arr,arr+5);
+//	sort(v.begin(),v.end());
+//	
+//	for(int i=0; i<v.size(); i++)
+//	{
+//		if(v[i] == 1)
+//		{
+//			c++;
+//		}
+//	}
+//	cout<<c;
 	
 //	for(auto x:v)
 //	{
