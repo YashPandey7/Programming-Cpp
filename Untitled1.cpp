52dcb61a6d52151d3460f414bc71b9e58ec00084
Yash Pandey <yashdevesh10@gmail.com>
2023-02-07 12:59:03 +0530

    adding

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,37 +1,26 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
-
-int fun(vector <int>& digits)
-{
-	
-		int idx=digits.size()-1;
-        while(idx>=0)
-        {
-            if(digits[idx]==9)
-            {
-                digits[idx]=0;
-            }
-            else
-            {
-                digits[idx]+=1;
-            }
-            idx--;
-        }
-        digits.insert(digits.begin(),1);
-	
-	for(auto x:digits)
-	{
-		cout<<x<<" ";
-	}
-	
-	return 0;
-}
 
 int main()
 {
-	vector <int> v = {0,9,9,9,9};
-	fun(v);
+	vector <int> v;
+	v.insert(v.begin(),{1,3,2,6,5,4});
+	sort(v.begin(),v.end());
 	
+	auto it = find(v.begin(),v.end(), 5);
+	if(it==v.end())
+		cout<<"not found";
+	else
+		cout<<"Found at index = "<<(it-v.begin());
+	
+	cout<<endl;
+	it= lower_bound(v.begin(),v.end(), 2.5);
+	cout<<*it;
+	
+	cout<<endl;
+	for(auto x:v)
+		cout<<x<<" ";
 	return 0;
 }
