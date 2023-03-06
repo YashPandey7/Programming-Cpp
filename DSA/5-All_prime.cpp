c28094937de4a132e813bfb629a58aeed1690cb8
Yash Pandey <yashdevesh10@gmail.com>
2023-03-06 22:06:47 +0530

    all prime numbers

---
+++ DSA/5-All_prime.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+bool prime(int n)
+{
+	if(n==1)
+		return false;
+		
+	if(n==2 || n==3)
+		return true;
+		
+	if(n%2==0 || n%3==0)
+	{
+		return false;
+	}
+	
+	for(int i=5; i*i<=n; i=i+6)
+	{
+		if(n%i==0 && n%(i+2)==0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void print(int n)
+{
+	for(int i=1; i<n;i++)
+	{
+		if(prime(i)==true)
+		{
+			cout<<i<<" ";
+		}
+	}
+}
+
+int main()
+{
+	print(10);
+	
+	return 0;
+}
