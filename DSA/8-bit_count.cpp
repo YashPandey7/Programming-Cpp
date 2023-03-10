b4f20194748ade29926d8cf2de0c744fd89319a5
Yash Pandey <yashdevesh10@gmail.com>
2023-03-10 12:23:27 +0530

    bit

--- DSA/8-bit_count.cpp
+++ DSA/8-bit_count.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
 using namespace std;
 
+//int count(int n)
+//{
+//	int res=0;
+//	for(int i=1;i<=n;i++)
+//	{
+//		int x=i;
+//		while(x>0)
+//		{
+//			x=x&(x-1);
+//			res++;
+//		}
+//	}
+//	return res;
+//}
+int tbl[256];
+void init()
+{
+	tbl[0]=0;
+	for(int i=1; i<=255; i++)
+		tbl[i] = tbl[i&(i-1)] + 1;
+}
 int count(int n)
 {
-	int res=0;
-	for(int i=1;i<=n;i++)
-	{
-		int x=i;
-		while(x>0)
-		{
-			x=x&(x-1);
-			res++;
-		}
-	}
-	return res;
+	return tbl[n&255] + tbl[(n>>8)&255] + tbl[(n>>16)&255] + tbl[(n>>24)];
 }
+
 
 int main()
 {
-	cout<<count(4);
+//	cout<<count(4);
+	init();
+	int n=9;
+	cout<<count(n)<<endl;
+	cout<<count(7);
 	
 	return 0;
 }
