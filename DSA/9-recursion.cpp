1ea00e2e477e56f3fe615bb58e65029ec9b1041d
Yash Pandey <yashdevesh10@gmail.com>
2023-03-11 22:30:43 +0530

    recursion

--- DSA/9-recursion.cpp
+++ DSA/9-recursion.cpp
@@ -15,5 +15,14 @@ int main()
 	cout<<"Binary conversion of 13 -- ";
 	fun(13);
 	
+	cout<<"\nBinary conversion of 17 -- ";
+	fun(17);
+	
+	cout<<"\nBinary conversion of 19 -- ";
+	fun(19);
+	
+	cout<<"\nBinary conversion of 1231 -- ";
+	fun(1231);
+	
 	return 0;
 }
