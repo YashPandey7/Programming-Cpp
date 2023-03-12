7c078c875970c135c4bafa95ec9d13addd081a01
Yash Pandey <yashdevesh10@gmail.com>
2023-03-12 22:26:58 +0530

    recursion

--- DSA/10-recursion.cpp
+++ DSA/10-recursion.cpp
@@ -10,9 +10,23 @@ void fun(int n)
 	fun(n-1);
 }
 
+int sum(int n)
+{
+	if(n==0)
+		return 0;
+		
+	else
+		return sum(n/10) + n%10;
+}
+
 int main()
 {
 	fun(4);
+	cout<<"\nSum of 123 is ";
+	cout<<sum(123);
+	
+	cout<<"\nSum of 1232345 is ";
+	cout<<sum(1232345);
 	
 	return 0;
 }
