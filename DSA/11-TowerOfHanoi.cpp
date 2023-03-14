3f3daf6e9e9720febe26925eea6011caf79fd092
Yash Pandey <yashdevesh10@gmail.com>
2023-03-14 13:21:44 +0530

    Tower of hanoi

--- DSA/11-TowerOfHanoi.cpp
+++ DSA/11-TowerOfHanoi.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
 
+void toh(int n, char A, char B, char C)
+{
+	if(n==0)
+	{
+		return ;
+//		cout<<"move disk 1 from "<<A<<" to "<<C<<endl;
+	}
+	
+	toh(n-1,A,C,B);
+	cout<<"move disk "<<n<<" from "<<A<<" to "<<C<<endl;
+	toh(n-1,B,A,C);
+}
+
 int main()
 {
+	int n=3;
+	toh(n,'A','B','C');
 	
 	return 0;
 }
