878afa9542943e33c8bf2a316d3cc7d7edc5a269
Yash Pandey <yashdevesh10@gmail.com>
2023-03-31 14:19:49 +0530

    14-Move_Zeros

---
+++ DSA/15-LeftRotate.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int leftRotate(int arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		int temp=arr[i];
+		arr[i]=arr[i+3];
+		arr[i+3]=temp;
+	}
+	
+	for(int i=0; i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main()
+{
+	int arr[] = {1,2,3,4,5};
+	leftRotate(arr,5);
+	
+	return 0;
+}
