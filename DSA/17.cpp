0e2fa179801e6abbfb29bea39fd634dff628a23d
Yash Pandey <yashdevesh10@gmail.com>
2023-04-06 20:21:25 +0530

    Adding Files

--- DSA/17.cpp
+++ DSA/17.cpp
@@ -3,23 +3,28 @@ using namespace std;
 
 int count(bool arr[],int n)
 {
-	int x=0 ,y=0 ;
+	int freq=1, x=0;
 	for(int i=0; i<n; i++)
 	{
-		if(arr[i] == 1)
-			x++;
-			if(y<x)
-				y=x;
-			
-		else 
-			x=0;
+		if(arr[i]==1 && arr[i+1]==1)
+		{
+			freq++;
+			if(x<freq)
+			{
+				x=freq;
+			}
+		}
+		else
+		{
+			freq=1;
+		}
 	}
-	return y;
+	return x;
 }
 
 int main()
 {
-	bool arr[] = {0,1,1,1,0,0,1,1,1,1,0};
+	bool arr[] = {0,1,1,1,0,0,1,1,1,1,0,1};
 	int n=sizeof(arr)/sizeof(arr[0]);
 	
 	cout<<count(arr,n);
