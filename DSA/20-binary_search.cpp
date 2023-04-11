fb27d7e84f7133a35b689aeb20bb24a707ec348f
Yash Pandey <yashdevesh10@gmail.com>
2023-04-11 12:25:09 +0530

    Adding Files

--- DSA/20-binary_search.cpp
+++ DSA/20-binary_search.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main()
 {
 	vector <int> v {10,20,30,40,50};
-	vector<int>::iterator upper1,upper2;
-	upper1 = upper_bound(v.begin(),v.end(),50);
+	vector<int>::iterator upper1,lower1;
+	upper1 = upper_bound(v.begin(),v.end(),41);
 	cout<<(upper1 - v.begin());
+	
+	lower1 = lower_bound(v.begin(), v.end(), 41);
+	cout<<endl<<(lower1 - v.begin());
 	
 	return 0;
 }
