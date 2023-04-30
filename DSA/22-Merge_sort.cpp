af1dcc6d6931d41d159cdd53e78bff03f0b55598
Yash Pandey <yashdevesh10@gmail.com>
2023-04-30 21:40:41 +0530

    Adding merge sort

--- DSA/22-Merge_sort.cpp
+++ DSA/22-Merge_sort.cpp
@@ -1,8 +1,55 @@
 #include<iostream>
 using namespace std;
 
+int merge(int arr[], int l, int mid, int h)
+{
+	int n1 = mid - l + 1;
+	int n2 = h - mid ;
+	int a[n1], b[n2];
+	for(int i=0; i<n1; i++)
+	{
+		a[i] = arr[l + i];
+	}
+	for(int i=0; i<n2; i++)
+	{
+		b[i] = arr[mid + 1 + i];
+	}
+	
+	int i=0, j=0, k=l;
+	while(i<n1 && j<n2)
+	{
+		if(a[i] <= b[j])
+		{
+			arr[k] = a[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			arr[k] = b[j];
+			j++;
+			k++;
+		}
+	}
+	
+}
+
+int mergeSort(int arr[], int l, int h)
+{
+	int mid = l + (h-l)/2;
+	mergeSort(arr, l, mid);
+	mergeSort(arr, mid+1, h);
+	merge(arr,l, mid, h);
+}
+
 int main()
 {
+	int arr[] = {5,3,2,8,9,4};
+	mergeSort(arr, 0, 5);
+	for(int i=0; i<6; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
 	
 	return 0;
 }
