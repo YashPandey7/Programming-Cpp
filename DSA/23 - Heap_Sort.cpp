b278e04220a04e9f23b05bcca9985bc94750daff
Yash Pandey <yashdevesh10@gmail.com>
2023-06-11 12:30:19 +0530

    Adding files

--- DSA/23 - Heap_Sort.cpp
+++ DSA/23 - Heap_Sort.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void maxHeapify(int arr[], int n, int i)
+{
+	int largest = i, left = 2*i+1, right = 2*i+2;
+	if(left<n && arr[largest] < arr[left])
+	{
+		largest = left;
+	}
+	if(right<n && arr[largest] < arr[right])
+	{
+		largest = right;
+	}
+	
+	if(largest != i)
+	{
+		swap(arr[i], arr[largest]);
+		maxHeapify(arr,n,i);
+	}
+}
 
+void buildHeap(int arr[], int n)
+{
+	
+}
 
 int main()
 {
