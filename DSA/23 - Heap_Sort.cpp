e7c63e4f816a053a04e79b73f37a6f312d69e287
Yash Pandey <yashdevesh10@gmail.com>
2023-06-11 12:35:02 +0530

    Heap Sort

--- DSA/23 - Heap_Sort.cpp
+++ DSA/23 - Heap_Sort.cpp
@@ -22,11 +22,31 @@ void maxHeapify(int arr[], int n, int i)
 
 void buildHeap(int arr[], int n)
 {
-	
+	for(int i=(n-2)/2; i>=0; i--)
+	{
+		maxHeapify(arr, n, i);
+	}
+}
+
+void heapSort(int arr[] , int n)
+{
+	buildHeap(arr, n);
+	for(int i=n-1; i>=1; i--)
+	{
+		swap(arr[i], arr[0]);
+		maxHeapify(arr, i, 0);
+	}
 }
 
 int main()
 {
+	int arr[5] = {3,2,1,9,8};
+	heapSort(arr, 5);
+	
+	for(int i=0; i<5; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
 	
 	return 0;
 }
