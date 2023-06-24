939e03cc5f1c203f033644e300eb6987ef257b69
Yash Pandey <yashdevesh10@gmail.com>
2023-06-24 14:24:19 +0530

    Linked List

--- DSA/25-LinkedList.cpp
+++ DSA/25-LinkedList.cpp
@@ -12,6 +12,16 @@ struct Node
 	}
 };
 
+void printList(Node *head)
+{
+	Node *curr = head;
+	while(curr != NULL)
+	{
+		cout<<curr->data<<" ";
+		curr = curr->next;
+	}
+}
+
 int main()
 {
 	Node *head = new Node(10);
@@ -19,5 +29,7 @@ int main()
 	head->next->next = new Node(30);
 	head->next->next->next = new Node(40);
 	head->next = temp;
+	
+	printList(head);
 	return 0;
 }
