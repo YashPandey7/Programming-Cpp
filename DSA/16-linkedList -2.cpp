a8c69be133e46fb2b1de41c619b99c0fa02f939a
Yash Pandey <yashdevesh10@gmail.com>
2023-06-25 12:17:17 +0530

    Linked List

--- DSA/16-linkedList -2.cpp
+++ DSA/16-linkedList -2.cpp
@@ -12,11 +12,22 @@ struct Node
 	}
 };
 
+Node *traverse(Node *head)
+{
+	while(head != NULL)
+	{
+		cout<<head->data<<" ";
+		head = head->next;
+	}
+}
+
 int main()
 {
 	Node *head = new Node(5);
 	head->next = new Node(10);
 	head->next->next = new Node(15);
+	head->next->next->next = new Node(20);
+	traverse(head);
 	
 	return 0;
 }
