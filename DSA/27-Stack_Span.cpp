18bfe49a608a917da2aa4ad28b6dca3bb2df86e7
Yash Pandey <yashdevesh10@gmail.com>
2023-07-04 13:16:51 +0530

    27 - Stack_Span

--- DSA/27-Stack_Span.cpp
+++ DSA/27-Stack_Span.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 void printSpan(int arr[], int n)
 {
-	
+	stack<int> s;
+	s.push(0);
+	cout<<1<<" ";
+	int span;
+	for(int i=1; i<n; i++)
+	{
+		while(s.empty() == false && arr[s.top()]<= arr[i])
+		{
+			s.pop();
+		}
+		
+		if(s.empty() == true)
+		{
+			span = i+1;
+		}
+		else
+		{
+			span = i - s.top();
+		}
+		cout<<span<<" ";
+		s.push(i);
+	}
 }
 
 int main()
