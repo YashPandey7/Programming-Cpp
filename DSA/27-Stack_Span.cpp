38129a1172cb37e6c6178c987eaacfdb6ad4ae1d
Yash Pandey <yashdevesh10@gmail.com>
2023-07-04 13:11:51 +0530

    27 - Stack_Span

---
+++ DSA/27-Stack_Span.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void printSpan(int arr[], int n)
+{
+	
+}
+
+int main()
+{
+	int arr[] = {60,10,20,40,35,38,50,70,65};
+	int n = sizeof(arr)/sizeof(arr[0]);
+//	cout<<n;
+
+	printSpan(arr, n);
+	
+	return 0;
+}
