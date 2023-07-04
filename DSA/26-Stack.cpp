e9279935d60b3bb5efb9bd650008c5e28886d957
Yash Pandey <yashdevesh10@gmail.com>
2023-07-04 13:02:22 +0530

    26 - Stack

--- DSA/26-Stack.cpp
+++ DSA/26-Stack.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
 #include<stack>
+#include < vector>
+using namespace std;
+
+struct Stack{
+	vector<int> v;
+	
+	void push(int x)
+	{
+		v.push_back(x);
+	}
+	int pop()
+	{
+		int x = v.back();
+		v.pop_back();
+		return x;
+	}
+	
+};
+
+int main()
+{
+	Stack
+	
+	return 0;
+}
