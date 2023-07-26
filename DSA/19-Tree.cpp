df6f6aea4af7e90a57c6c2a2da8034eeeb08b70c
Yash Pandey <yashdevesh10@gmail.com>
2023-07-26 13:10:52 +0530

    29-tree-BFS

--- DSA/19-Tree.cpp
+++ DSA/19-Tree.cpp
@@ -34,7 +34,6 @@ void levelTraversal(Node *root)
 	while(!q.empty())
 	{
 		Node *curr = q.front();
-//		cout<<q.front()->key<<" ";
 		q.pop();
 		cout<<curr->key<<" ";
 		if(curr->left)
@@ -48,6 +47,37 @@ void levelTraversal(Node *root)
 	}
 }
 
+void levelNewLine(Node *root)
+{
+	queue<Node *> q;
+	if(root == NULL)
+		return;
+		
+	q.push(root);
+	q.push(NULL);
+	
+	while(q.size() > 1)
+	{
+		Node *curr = q.front();
+		q.pop();
+		
+		if(curr == NULL)
+		{
+			cout<<endl;
+			q.push(NULL);
+		}
+		else
+		{
+			cout<<curr->key<<" ";
+			if(curr->left)
+				q.push(curr->left);
+			if(curr->right)
+				q.push(curr->right);
+		}
+	}
+}
+
+
 int main()
 {
 	Node *root = new Node(5);
@@ -55,11 +85,17 @@ int main()
 	root->right = new Node(15);
 	root->left->left = new Node(20);
 	root->left->right = new Node(25);
+	root->right->left = new Node(30);
+	root->right->right = new Node(35);
 	
 	cout<<"Inorder DFS -->  ";
 	inorder(root);
 	
 	cout<<endl<<"BFS -->  ";
 	levelTraversal(root);
+	
+	cout<<"\nBFS -->  \n";
+	levelNewLine(root);
+	
 	return 0;
 }
