2edca4d01cd1f1b60c3b123dc38b21078c2daf7f
Yash Pandey <yashdevesh10@gmail.com>
2023-07-26 12:52:18 +0530

    29-tree-inorder

--- DSA/19-Tree.cpp
+++ DSA/19-Tree.cpp
@@ -12,12 +12,25 @@ struct Node {
 	}
 };
 
+void inorder(Node *root)
+{
+	if(root == NULL)
+	{
+		return ;
+	}
+	inorder(root->left);
+	cout<<root->key<<" ";
+	inorder(root->right);
+}
+
 int main()
 {
 	Node *root = new Node(5);
 	root->left = new Node(10);
 	root->right = new Node(15);
 	root->left->left = new Node(20);
+	root->left->right = new Node(25);
 	
+	inorder(root);
 	return 0;
 }
